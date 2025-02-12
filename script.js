5c6d8e8728401a393937f164416b11031f81f45d
AA-lab-ctrl <alallaire@students.wcpss.net>
2025-02-12 08:26:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// Wait for the page to fully load
 document.addEventListener('DOMContentLoaded', () => {
     const reasons = document.querySelectorAll('.reason');
+
+    // Add a click event to each reason
     reasons.forEach(reason => {
         reason.addEventListener('click', () => {
             alert(`Yeah, Asher, we all know: ${reason.textContent}!`);
